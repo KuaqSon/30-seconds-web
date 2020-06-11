091f7bfccfebf945bd9902d993d9c5b8b7e96a58
Chalarangelo <chalarangelo@gmail.com>
2020-06-11 21:49:09 +0300

    WIP - Process images outside of Gatsby

--- src/build/parsers/index.js
+++ src/build/parsers/index.js
@@ -4,6 +4,7 @@ import parseRequirables from './parseRequirables';
 import parseTemplates from './parseTemplates';
 import parseListingMetas from './parseListingMetas';
 import parseSnippets from './parseSnippets';
+import parseImages from './parseImages';
 
 export {
   parseQueries,
@@ -11,5 +12,6 @@ export {
   parseRequirables,
   parseTemplates,
   parseListingMetas,
+  parseImages,
   parseSnippets
 };
